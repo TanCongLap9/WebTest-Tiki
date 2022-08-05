fdfdf1b50ed527316b47eade0301e2ceebea3efa
TanCongLap9 <webby4664@gmail.com>
2022-08-05 19:33:15 +0700

    2 files modified, 4 added, 4 deleted

--- components/header.js
+++ components/header.js
@@ -1,23 +1,23 @@
 import * as React from "react";
-const theLoai = fetch(String.raw`https://62ecdb67818ab252b603f9f3.mockapi.io\header__theloai`).then(response => response.json()).then(data => console.log(data));
+let theLoai;
+fetch(String.raw`https://62ecdb67818ab252b603f9f3.mockapi.io\header__theloai`).then(response => response.json()).then(json => {theLoai = json})
 
 
-
-/*const Header = () => (
+const Header = () => (
   <div className="header">
     <img />
-    <input type="search"/>
-    <input type="submit"/>
+    <input className="header__timkiem" type="search" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn..."/>
+    <input className="header__nuttimkiem" type="submit" value="Tìm kiếm"/>
     <div className="header__dangnhap"></div>
     <div className="header__giohang"></div>
     <button className="header__banhang"></button>
     <table className="header__theloai">
       <tbody><tr>
-      {theLoai.map(obj => (
-        <td></td>
-      ))}
+      {
+        undefined
+      }
       </tr></tbody>
     </table>
   </div>
 );
-export default Header;*/
+export default Header;
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Header from '../components/header.js';
+import './style.sass';
+
+const App = () => (
+  <>
+    <Header />
+  </>
+);
+export default App;
