f20320ea7413b241e6359779a1a49014fc6a82f3
TanCongLap9 <webby4664@gmail.com>
2022-08-06 01:36:26 +0700

    2 files modified

--- components/header.js
+++ components/header.js
@@ -4,20 +4,38 @@ fetch(String.raw`https://62ecdb67818ab252b603f9f3.mockapi.io\header__theloai`).t
 
 
 const Header = () => (
-  <div className="header">
-    <img />
-    <input className="header__timkiem" type="search" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn..."/>
-    <input className="header__nuttimkiem" type="submit" value="Tìm kiếm"/>
-    <div className="header__dangnhap"></div>
-    <div className="header__giohang"></div>
-    <button className="header__banhang"></button>
-    <table className="header__theloai">
-      <tbody><tr>
-      {
-        undefined
-      }
-      </tr></tbody>
-    </table>
+  <div className="header-bg">
+    <div className="header">
+      <div className="logo">
+        <img className="logo-tiki" src="https://salt.tikicdn.com/ts/upload/ae/f5/15/2228f38cf84d1b8451bb49e2c4537081.png" alt="Tiki"/>
+        <br/>
+        <img className="logo-freeship" src="https://salt.tikicdn.com/ts/upload/e5/1d/22/61ff572362f08ead7f34ce410a4a6f96.png" alt="Freeship"/>
+      </div>
+      <input className="timkiem" type="search" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn..."/>
+      <input className="nuttimkiem" type="submit" value="Tìm kiếm"/>
+      <div className="dangnhap">
+        <img src="https://salt.tikicdn.com/ts/upload/67/de/1e/90e54b0a7a59948dd910ba50954c702e.png"/>
+        <div>
+          <p>Đăng nhập / Đăng ký</p>
+          <p>Tài khoản▼</p>
+        </div>
+      </div>
+      <div className="giohang">
+        <img src="https://salt.tikicdn.com/ts/upload/40/44/6c/b80ad73e5e84aeb71c08e5d8d438eaa1.png"/>
+        <p>Giỏ hàng</p>
+      </div>
+      <button className="banhang">
+        <img src="https://frontend.tikicdn.com/_desktop-next/static/img/icon-seller.svg"/>
+        Bán hàng cùng Tiki
+      </button>
+      <table className="theloai">
+        <tbody><tr>
+        {
+          console.log(theLoai)
+        }
+        </tr></tbody>
+      </table>
+    </div>
   </div>
 );
 export default Header;
