a0ae21663e203153a29c802eb4e1157a3a806502
TanCongLap9 <109658401+TanCongLap9@users.noreply.github.com>
2022-09-16 01:25:51 +0700

    2 files modified, 1 added, 1 deleted

---
+++ components/banner_row_1-3.jsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import axios from 'axios';
+import * as ReactQuery from 'react-query';
+const fetcher = (url) => fetch(url).then((response) => response.json());
+const queryClient = new ReactQuery.QueryClient();
+
+const BannersRow1 = () => {
+  const { data: banner14Items } = ReactQuery.useQuery(
+      ['banners14Items'],
+      fetcher.bind(
+        undefined,
+        'https://62ecdb67818ab252b603f9f3.mockapi.io/data/banner14Items'
+      )
+    ),
+    { data: hoan50kxu } = ReactQuery.useQuery(
+      ['hoan50kXu'],
+      fetcher.bind(
+        undefined,
+        'https://62ecdb67818ab252b603f9f3.mockapi.io/data/hoan50kxu'
+      )
+    );
+  if ([banner14Items, hoan50kxu].some((v) => v === undefined))
+    return <p>Loading</p>;
+  return (
+    <div className="banners_row_1 widthlimit">
+      <div className="banners_row_1__14_items">
+        {banner14Items?.data.map(({ src: imageUrl }, index) => (
+          <img
+            className="banners_row_1__item"
+            src={imageUrl}
+            key={`banners_row_1_item${index}`}
+          />
+        ))}
+      </div>
+      <img className="banners_row_1__hoan50kxu" src={hoan50kxu.src} />
+    </div>
+  );
+};
+const BannersRow2 = () => {
+  const { data: giaSocHomNay } = ReactQuery.useQuery(
+      ['giaSocHomNay'],
+      fetcher.bind(
+        undefined,
+        'https://62ecdb67818ab252b603f9f3.mockapi.io/data/giaSocHomNay'
+      )
+    ),
+    { data: banner5Items } = ReactQuery.useQuery(
+      ['banner5Items'],
+      fetcher.bind(
+        undefined,
+        'https://62ecdb67818ab252b603f9f3.mockapi.io/data/banner5Items'
+      )
+    );
+  if ([giaSocHomNay, banner5Items].some((v) => v === undefined))
+    return <p>Loading</p>;
+  return (
+    <div className="banners_row_2 widthlimit">
+      <div className="banners_row_2__giasochomnay">
+        <table><tr>
+          {banner14Items?.data.map(({ src: imageUrl }, index) => (
+            <img
+              className="banners_row_2__mathang"
+              src={imageUrl}
+              key={`banners_row_2_item${index}`}
+            />
+          ))}
+        </tr></table>
+      </div>
+      <div className="banners_row_2__banner5items">
+        <table><tr>
+        {banner5Items?.data.map(({ image: imageUrl, video: videoUrl }, index) => (
+          <td
+            className="banners_row_2__item"
+            key={`banners_row_2_item${index}`}
+          >{video?
+            <video><source src={videoUrl}></source></video>
+          : <img src={imageUrl}/>}</td>
+        ))}</tr></table></div>
+    </div>
+  );
+};
+
+const BannersRows123 = () => (
+  <ReactQuery.QueryClientProvider client={queryClient}>
+    <BannersRow1 />
+  </ReactQuery.QueryClientProvider>
+);
+exports.default = BannersRow1;
--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import CacTheLoai from '../components/cac_the_loai';
 import GoiYHomNay from '../components/goi_y_hom_nay';
-import BannerRow1 from "../components/banner_row_1";
+import BannerRows123 from "../components/banner_row_1-3";
 import './style.scss';
 
 const App = () => (
   <>
-    <BannerRow1 />
+    <BannerRows123 />
   </>
 );
 export default App;
